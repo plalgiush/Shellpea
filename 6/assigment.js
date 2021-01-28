d5314494a4a8df32bf04fa5665a5ceeac031f16f
plalgiush <ilyasushko@yandex.ru>
2021-01-28 22:21:08 +0300

    Homework #6

---
+++ 6/assigment.js
@@ -0,0 +1,11 @@
+const changeColor = document.getElementById('task-1');
+const changeColorText = document.querySelector('#task-1');
+
+changeColor.style.backgroundColor = 'black';
+changeColorText.classList.add('text-color');
+
+document.querySelector('head title').text = 'Домашнее задание сделано!';
+document.head.querySelector('title').text = 'Домашнее задание сделано!';
+
+
+document.querySelector('h1').innerText = 'Домашнее задание сделано!';
