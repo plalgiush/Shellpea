c997ad1d97b9870a824e7d177df968312d08701d
plalgiush <ilyasushko@yandex.ru>
2021-01-28 22:13:05 +0300

    Homework #2

---
+++ 2/assignment.js
@@ -0,0 +1,21 @@
+function sayHi() {
+    alert("HI");
+};
+sayHi();
+
+function Name(name) {
+    alert(name);
+}
+
+Name("ILYA");
+
+
+document.getElementById('task-3').onclick = function () {
+    alert("success");
+};
+
+function threeArguments(first, second, third) {
+    alert(first + second + third);
+}
+
+threeArguments("Hello", " my dear ", "friend");
