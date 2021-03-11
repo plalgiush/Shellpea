8e0a9c156efbb587e5fc124b2ee4dc85c68104da
plalgiush <ilyasushko@yandex.ru>
2021-03-11 11:04:59 +0300

    todo almost with drag and drop

--- 12/assets/scripts/app.js
+++ 12/assets/scripts/app.js
@@ -5,22 +5,55 @@ class Project {
         this.button.addEventListener('click',this.showInfo); 
     };
 
-    showInfo() {
-        const li = document.querySelectorAll('li');
-        li.forEach(li => {
+    showInfo(e) {
+        // console.log(e);  
+        let p = e.path[1].id;
+        // console.log(p);
+        if (p === "p1") {
+            let j = 1;
+            const showLi = document.getElementById(`p${j}`);
+            // console.log(showLi); 
             let div = document.createElement('div');
-            div.innerHTML = li.dataset.extraInfo; 
+            div.innerHTML = showLi.dataset.extraInfo; 
             div.setAttribute('class', 'card');
-            li.append(div);
-            console.log(div);
+            showLi.append(div);
+            // console.log(div);
             div.addEventListener('click', () => {
                 div.parentNode.removeChild(div);
             })
             div.style.marginTop = "10px";
             div.style.padding = "10px";
-        })
-           
-    };
+        } else if (p === "p2") {
+            let j = 2;
+            const showLi = document.getElementById(`p${j}`);
+            // console.log(showLi);
+            let div = document.createElement('div');
+            div.innerHTML = showLi.dataset.extraInfo; 
+            div.setAttribute('class', 'card');
+            showLi.append(div);
+            // console.log(div);
+            div.addEventListener('click', () => {
+                div.parentNode.removeChild(div);
+            })
+            div.style.marginTop = "10px";
+            div.style.padding = "10px"; 
+        } else if (p === "p3") {
+            let j = 3; 
+            const showLi = document.getElementById(`p${j}`);
+            // console.log(showLi);
+            let div = document.createElement('div');
+            div.innerHTML = showLi.dataset.extraInfo; 
+            div.setAttribute('class', 'card');
+            showLi.append(div);
+            // console.log(div);
+            div.addEventListener('click', () => {
+                div.parentNode.removeChild(div);
+            })
+            div.style.marginTop = "10px";
+            div.style.padding = "10px";    
+        }
+        
+    }
 
 };
 
@@ -41,7 +74,7 @@ class ActivateProject extends Project {
         };        
 
     };
-    
+
 };
 
 class FinishProject extends Project {
@@ -49,18 +82,81 @@ class FinishProject extends Project {
         super(li);
         this.li = li;
         this.finishBtn();
+        this.dragItems();
+        this.dropZones();
     };
 
     finishBtn() {this.li.querySelector('button:last-of-type').onclick = () => {
-        console.log('finish');
+        console.log('finish'); 
         this.li.remove();
         document.querySelector('#active-projects ul').append(this.li);
         this.li.querySelector('button:last-of-type').innerHTML = "Finish";
         this.li.querySelector('button:last-of-type').classList.add('finish-btn');
         new ProjectList('active');
         };        
+    };
+
+    dragItems() {
+        const li = document.querySelectorAll('li');
+        li.forEach(dragItem => {
+            dragItem.addEventListener('dragstart', handlerDragstart)
+            dragItem.addEventListener('dragend', handlerDragend)
+            dragItem.addEventListener('drag', handlerDrag)
+        })
+
+        function handlerDragstart(event) {
+            console.log(event);
+            setTimeout(() => {
+                this.classList.add('hide');
+            },0);
+            event.dataTransfer.setData("dragItem", this.dataset.extraInfo);
+            console.log('dragstart', this);
+        }
+
+        function handlerDragend(event) {
+            console.log('dragend', this);
+            this.classList.remove('hide');
+        }
+
+        function handlerDrag(event) {
+            console.log('drag');
+        }
 
     };
+
+    dropZones() {
+        const ul = document.querySelectorAll('ul');
+        ul.forEach(dropZone => {
+            dropZone.addEventListener('dragenter', handlerDragenter)
+            dropZone.addEventListener('dragleave', handlerDragleave)
+            dropZone.addEventListener('dragover', handlerDragover)
+            dropZone.addEventListener('drop', handlerDrop)
+        })
+
+        function handlerDragenter(event) {
+            console.log('dragenter', this);
+            this.classList.add('hovered');
+            event.preventDefault();
+        }
+
+        function handlerDragleave(event) {
+            console.log('dragleave', this);
+            this.classList.remove('hovered');
+        }
+
+        function handlerDragover(event) {
+            // console.log('dragover', this);
+            event.preventDefault();
+        }
+
+        function handlerDrop(event) {
+            let dragItem = event.dataTransfer.getData("dragItem");
+            console.log(event.dataTransfer.getData("dragItem"));
+            // console.log('drop', this);
+            this.append(dragItem);
+            this.classList.remove('hovered');
+        }
+    }
 };
 
 class ProjectList {
@@ -85,3 +181,71 @@ class App {
 };
 
 App.init();
+
+
+// const dragAndDrop = () => {
+
+//     const li = document.querySelectorAll('li');
+//     const ul = document.querySelectorAll('ul');
+
+//     li.forEach(dragItem => {
+//         dragItem.addEventListener('dragstart', handlerDragstart)
+//         dragItem.addEventListener('dragend', handlerDragend)
+//         dragItem.addEventListener('drag', handlerDrag)
+//     })
+
+//     function handlerDragstart(event) {
+//         console.log(event);
+//         setTimeout(() => {
+//             this.classList.add('hide');
+//         },0);
+//         event.dataTransfer.setData("dragItem", this.dataset.extraInfo);
+//         console.log('dragstart', this);
+//     }
+
+//     function handlerDragend(event) {
+//         console.log('dragend', this);
+//         this.classList.remove('hide');
+//     }
+
+//     function handlerDrag(event) {
+//         console.log('drag');
+//     }
+
+
+
+    
+//     ul.forEach(dropZone => {
+//         dropZone.addEventListener('dragenter', handlerDragenter)
+//         dropZone.addEventListener('dragleave', handlerDragleave)
+//         dropZone.addEventListener('dragover', handlerDragover)
+//         dropZone.addEventListener('drop', handlerDrop)
+//     })
+
+//     function handlerDragenter(event) {
+//         console.log('dragenter', this);
+//         this.classList.add('hovered');
+//         event.preventDefault();
+//     }
+
+//     function handlerDragleave(event) {
+//         console.log('dragleave', this);
+//         this.classList.remove('hovered');
+//     }
+
+//     function handlerDragover(event) {
+//         // console.log('dragover', this);
+//         event.preventDefault();
+//     }
+
+//     function handlerDrop(event) {
+//         let dragItem = event.dataTransfer.getData("dragItem");
+//         console.log(event.dataTransfer.getData("dragItem"));
+//         // console.log('drop', this);
+//         this.append(dragItem);
+//         this.classList.remove('hovered');
+//     }
+
+// }
+
+// dragAndDrop();
